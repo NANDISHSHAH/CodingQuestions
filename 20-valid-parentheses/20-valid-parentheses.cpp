2a28bb48b75e3328a38af91f07fc7d38911313ca
Nandish Shah <32939305+NANDISHSHAH@users.noreply.github.com>
2022-03-14 00:35:25 +0530

    Time: 3 ms (35.76%), Space: 6.2 MB (80.05%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -3,25 +3,45 @@ public:
     bool isValid(string s) {
         stack<char>t;
         for(int i=0;i<s.length();i++){
-            if(s[i]=='(' || s[i] == '{' || s[i] == '[')
-            {
-             t.push(s[i]);
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                t.push(s[i]);
             }
             else if(s[i]==')'){
-                if(t.empty() or t.top()!='(' )return false;
+                if(t.empty() or t.top()!='(')return false ;
+                t.pop();
+            }
+             else if(s[i]=='}'){
+                if(t.empty() or t.top()!='{')return false ;
                 t.pop();
             }
             else if(s[i]==']'){
-                if(t.empty() or t.top()!='[' )return false;
+                if(t.empty() or t.top()!='[')return false ;
                 t.pop();
             }
-            else if(s[i]=='}'){
-                if(t.empty() or t.top()!='{' )return false;
-                t.pop();
-            }
-            
         }
-        if(t.empty()) return true;
+        if(t.empty())return true;
         else return false;
+//         stack<char>t;
+//         for(int i=0;i<s.length();i++){
+//             if(s[i]=='(' || s[i] == '{' || s[i] == '[')
+//             {
+//              t.push(s[i]);
+//             }
+//             else if(s[i]==')'){
+//                 if(t.empty() or t.top()!='(' )return false;
+//                 t.pop();
+//             }
+//             else if(s[i]==']'){
+//                 if(t.empty() or t.top()!='[' )return false;
+//                 t.pop();
+//             }
+//             else if(s[i]=='}'){
+//                 if(t.empty() or t.top()!='{' )return false;
+//                 t.pop();
+//             }
+            
+//         }
+//         if(t.empty()) return true;
+//         else return false;
     }
 };
